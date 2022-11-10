8e6e57d898ca18d8bc424bad3d55b950b8f45e7a
Brandon Apol <brandonapol@cedarville.edu>
2022-11-10 14:10:59 -0800

    parser complete

--- parser.js
+++ parser.js
@@ -1,7 +1,7 @@
-function parseExpression( program ){
+function parseExpression ( program ){
     program = skipSpace( program );
     let match, expr;
-    if ( match = /^"([^"]*)")/.exec( program )){ // Strings
+    if ( match = /^"([^"]*)"/.exec( program )){ // Strings
         expr = { type: "value", value: match[1] };
     } else if ( match = /^\d+\b/.exec( program )) { // Digits and word boundaries
         expr = { type: "value", value: Number(match[0]) };
@@ -14,8 +14,42 @@ function parseExpression( program ){
     return parseApply(expr, program.slice(match[0].length));
 } 
 
-function skipSpace(string) {
+function skipSpace (string) {
     let first = string.search(/\S/);
     if (first == -1) return "";
     return string.slice(first);
 }
+
+function parseApply (expr, program) {
+    program = skipSpace( program );
+    if (program[0] != "(") { // If the next character here isn't a parenthesis, 
+        // then it isn't an application (nothing is being called) so it just returns
+        return { expr: expr, rest: program }
+    }
+
+    program = skipSpace( program.slice(1) );
+    expr = { type: "apply", operator: expr, args: [] }
+    while ( program[0] != ")" ) {
+        let arg = parseExpression(program); // Recursive! Since parseExpression 
+        //calls parseApply. "Indirect recursion"
+        expr.args.push(arg.expr);
+        program = skipSpace(arg.rest);
+        if (program[0] == ",") {
+            program = skipSpace(program.slice(1));
+        } else if (program[0] != ")") {
+            throw new SyntaxError("Expected ',' or ')'");
+        }
+    }
+    return parseApply(expr, program.slice(1)) // recursive!
+}
+
+function parse(program) {
+    let {expr, rest} = parseExpression(program);
+    if (skipSpace(rest).length > 0) {
+      throw new SyntaxError("Unexpected text after program");
+    }
+    return expr;
+  }
+
+console.log(parse("+(a, 10)"));
+
